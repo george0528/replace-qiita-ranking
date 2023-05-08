f6235c3b0176b47783dee98e92d548b9cab2491f
jota <funayama.jota@gmail.com>
2023-05-09 02:47:37 +0900

    fix: build時にエラーが出ないように修正

--- frontend/app/about/detail/page.tsx
+++ frontend/app/about/detail/page.tsx
@@ -6,6 +6,7 @@ const Detail = () => {
     <>
       <h1>detailページ</h1>
       <Suspense fallback={<p className="mt-4">ユーザデータ　Loading...</p>}>
+        {/* @ts-expect-error Server Component */}
         <UserList />
       </Suspense>
     </>
